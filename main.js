b5014b8a8c23d4cc35516f60fb8d1c6f557b4c0c
mintiepnita <pnita@cca.edu>
2024-12-15 20:17:38 -0800

    Update main.js

--- main.js
+++ main.js
@@ -191,7 +191,7 @@ function printCatalog() {
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@400;700&display=swap" rel="stylesheet">
-            <title>Your Tab Catalog</title>
+            <title>My Tabs Citation</title>
             <style>
                 @media print {
                     @page {
